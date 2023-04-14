8fc037f8e80625ce0bf5f2013f899caed23e070e
recorner <89336832+recorner@users.noreply.github.com>
2023-04-14 22:36:24 +0300

    Create polygon.py
    Imports the necessary libraries: Web3 for connecting to a Polygon node, compile_source from solcx for compiling the smart contract source code, and json for working with JSON data.
    
    Connects to a Polygon node using the Web3 class and a node URL.
    
    Defines a contract_source string that contains the source code for a simple Solidity smart contract that stores and retrieves a message.
    
    Compiles the smart contract source code using the compile_source function from solcx. This produces a dictionary of compiled contracts.
    
    Extracts the ABI (Application Binary Interface) and bytecode from the compiled contract dictionary.
    
    Deploys the smart contract to the Polygon network using the constructor method of the Contract class. This sets the initial message value to "Hello, Polygon!".
    
    Waits for the transaction to be mined and returns the transaction receipt. This contains information about the newly deployed contract, including its address on the Polygon network.
    
    Interacts with the smart contract by creating an instance of the Contract class using the contract address and ABI.
    
    Retrieves the current message value using the getMessage function of the smart contract.
    
    Updates the message value using the setMessage function of the smart contract.
    
    Retrieves the updated message value using the getMessage function of the smart contract.
    
    Prints the current and updated message values to the console.

---
+++ polygon.py
@@ -0,0 +1,50 @@
+from web3 import Web3
+from solcx import compile_source
+import json
+
+# Connect to a Polygon node
+w3 = Web3(Web3.HTTPProvider('https://rpc-mainnet.maticvigil.com/'))
+
+# Compile the smart contract
+contract_source = """
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+}
+"""
+compiled_contract = compile_source(contract_source)
+
+# Deploy the smart contract
+abi = compiled_contract['<stdin>:HelloWorld']['abi']
+bytecode = compiled_contract['<stdin>:HelloWorld']['bin']
+contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+tx_hash = contract.constructor('Hello, Polygon!').transact({'from': w3.eth.accounts[0]})
+tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+contract_address = tx_receipt.contractAddress
+print(f"Contract deployed at address: {contract_address}")
+
+# Interact with the smart contract
+contract_instance = w3.eth.contract(address=contract_address, abi=abi)
+
+message = contract_instance.functions.getMessage().call()
+print(f"Current message: {message}")
+
+contract_instance.functions.setMessage('Hello, Matic!').transact({'from': w3.eth.accounts[0]})
+
+message = contract_instance.functions.getMessage().call()
+print(f"Updated message: {message}")
